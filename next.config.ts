7c3bb5604f63d88e6b3be30f5cd4b426b2fac80f
Halim Teguh S <63193828+HalimTeguh@users.noreply.github.com>
2026-05-26 14:59:35 +0700

    fix: Setup Github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/pbf-desktop-ui',
+  assetPrefix: '/pbf-desktop-ui',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
